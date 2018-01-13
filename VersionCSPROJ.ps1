c8a73e6820f25d3fc4b283c9379b089256cae11e
Chris Simmons <chris.dev@netchris.com>
2018-01-13 15:48:46 -0800

    Script is working as required

--- VersionCSPROJ.ps1
+++ VersionCSPROJ.ps1
@@ -1,27 +1,99 @@
 <#
-- Take argument, "-BuildNumber", an int
-- Take argument, "-csprojPath", a string
-- Get Project/PropertyGroup/Version from csproj
-  - Ensure this works with *Any* property group
-- Tokenize into Major, Minor, Patch version
-   -Major and Minor are ints, Patch version is a string
-- NumericPatch obtained by tokenizing Patch on "-" and taking [0]
-  - ensure this works with both "3-beta" and "3"
-- Inject build number into .NET versions: Major.Minor.Build.NumericPatch
-
+    Given -buildNumber and -csprojPath on the command line:
+    - Takes the SemVer format version in the csproj <Version> property and breaks out into Major, Minor, and Patch version
+    - Re-formats the AssemblyVersion and FileVersion as Major.Minor.BuildNumber.NumericPatch
+    - Exports the following into the environment:
+      - $env:semVer (e.g. "1.2.3-alpha")
+      - $env:majorVersion (e.g. "1" if semVer is "1.2.3-alpha")
+      - $env:minorVersion (e.g. "2" if semVer is "1.2.3-alpha")
+      - $env:patchVersion (e.g. "3-alpha" if semVer is "1.2.3-alpha")
+      - $env:patchVersionNumeric (e.g. "3" if semVer is "1.2.3-alpha")
+    - Saves AssemblyVersion, FileVersion, and InformationalVersion
 #>
 
 Param
 (
-    [Parameter(Mandatory)][int]$BuildNumber,
+    [Parameter(Mandatory)][int]$buildNumber,
     [Parameter(Mandatory)][string]$csprojPath
 )
 
-"Build number: $BuildNumber"
+"Build number: $buildNumber"
 
-"csproj = $csprojPath"
+$csprojFile = Get-Item $csprojPath
 
-if (!(Test-Path $csprojPath))
+if (!($csprojFile.Exists))
 {
-    Throw "csproj file """ + $csprojPath + """ does not exist."
+    Throw "csproj file """ + $csprojFile.FullName + """ does not exist."
 }
+
+$csprojFileFullName = $csprojFile.FullName
+
+"csproj = $csprojFileFullName"
+
+$xml = [xml](get-content $csprojPath)
+
+$propertyGroup = $xml.Project.PropertyGroup | Where {$_.Version}
+$version = $propertyGroup.Version
+$versionParts = $version.Split("{.}")
+
+[int]$majorVersion = $versionParts[0]
+[int]$minorVersion = $versionParts[1]
+[string]$patchVersion = $versionParts[2]
+[int]$patchVersionNumeric = $patchVersion.Split("{-}")[0]
+
+"Major version: $majorVersion"
+"Minor version: $minorVersion"
+"Patch version: $patchVersion"
+"Patch version (numeric part): $patchVersionNumeric"
+
+$assemblyVersion = "${majorVersion}.${minorVersion}.${buildNumber}.${patchVersionNumeric}"
+
+"New assembly/file version: $assemblyVersion"
+
+$assemblyVersionNode = $propertyGroup.AssemblyVersion
+$fileVersionNode = $propertyGroup.FileVersion
+$informationalVersionNode = $propertyGroup.InformationalVersion
+
+if($assemblyVersionNode)
+{
+    $propertyGroup.AssemblyVersion = $assemblyVersion
+}
+else
+{
+    "No AssemblyVersion property in the project.  Would have set to '${assemblyVersion}'"
+}
+
+if($fileVersionNode)
+{
+    $propertyGroup.FileVersion = $assemblyVersion
+}
+else
+{
+    "No FileVersion property in the project.  Would have set to '${assemblyVersion}'"
+}
+
+$informationalVersion = "Version $version (Build: $buildNumber)"
+
+if($informationalVersionNode)
+{
+    $propertyGroup.InformationalVersion = $informationalVersion
+}
+else
+{
+    "No InformationalVersion property in the project.  Would have set to '${informationalVersion}'"
+}
+
+# Clear environment variables first
+$env:semVer = ""
+$env:majorVersion = ""
+$env:minorVersion = ""
+$env:patchVersion = ""
+$env:patchVersionNumeric = ""
+
+$env:semVer = $version
+$env:majorVersion = $majorVersion
+$env:minorVersion = $minorVersion
+$env:patchVersion = $patchVersion
+$env:patchVersionNumeric = $patchVersionNumeric
+
+$xml.Save($csprojFile.FullName)
