0634a157001eebfb50bc4a0678f21443b45c9b36
Chris Simmons <chris.dev@netchris.com>
2018-01-13 14:29:59 -0800

    AppMetadata and tests

---
+++ src/NetChris.Core/NetChris.Core.UnitTests/AppMetadataTests.cs
@@ -0,0 +1,108 @@
+using FluentAssertions;
+using Xunit;
+
+namespace NetChris.Core.UnitTests
+{
+    public class AppMetadataTests
+    {
+        [Fact]
+        public void Automatic_ApplicationName_discern_should_work()
+        {
+            // Arrange
+            AppMetadata<AppMetadataTests> appMetadata = new AppMetadata<AppMetadataTests>("GroupNameDoesNotMatterForThisTest");
+            string expectedApplicationName = "NetChris.Core.UnitTests";
+
+            // Act
+            var applicationName = appMetadata.ApplicationName;
+
+            // Assert
+            applicationName.Should().Be(expectedApplicationName);
+        }
+
+        [Fact]
+        public void ApplicationGroup_should_flow_through()
+        {
+            // Arrange
+            string expectedApplicationGroup = "App.Group";
+            AppMetadata<AppMetadataTests> appMetadata = new AppMetadata<AppMetadataTests>("AppNameDoesNotMatterForThisTest", expectedApplicationGroup);
+
+            // Act
+            var applicationGroup = appMetadata.ApplicationGroup;
+
+            // Assert
+            applicationGroup.Should().Be(expectedApplicationGroup);
+        }
+
+        [Fact]
+        public void ApplicationVersion_is_formatted_correctly()
+        {
+            // Arrange
+            var applicationAssembly = typeof(AppMetadataTests).Assembly;
+            var version = applicationAssembly.GetName().Version;
+            var expectedVersion = $"1.2.3.0";
+
+            AppMetadata<AppMetadataTests> appMetadata =
+                new AppMetadata<AppMetadataTests>("DoesNotMatter");
+
+            // Act
+            var applicationVersion = appMetadata.GetApplicationVersion();
+
+            // Assert
+            applicationVersion.Should().Be(expectedVersion);
+        }
+
+        [Fact]
+        public void ExecutionInstanceId_should_stay_constant_over_instances()
+        {
+            // Arrange
+            AppMetadata<AppMetadataTests> appMetadata1 =
+                new AppMetadata<AppMetadataTests>("DoesNotMatter1");
+
+            AppMetadata<AppMetadataTests> appMetadata2 =
+                new AppMetadata<AppMetadataTests>("DoesNotMatter2");
+
+            // Act
+            var executionInstanceId1 = appMetadata1.GetExecutionInstanceId();
+            var executionInstanceId2 = appMetadata2.GetExecutionInstanceId();
+
+            // Assert
+            executionInstanceId1.Should().BeEquivalentTo(executionInstanceId2);
+        }
+
+        [Fact]
+        public void ExecutionInstanceTimestamp_should_stay_constant_over_instances()
+        {
+            // Arrange
+            AppMetadata<AppMetadataTests> appMetadata1 =
+                new AppMetadata<AppMetadataTests>("DoesNotMatter1");
+
+            AppMetadata<AppMetadataTests> appMetadata2 =
+                new AppMetadata<AppMetadataTests>("DoesNotMatter2");
+
+            // Act
+            var executionInstanceTimestamp1 = appMetadata1.GetExecutionInstanceTimestamp();
+            var executionInstanceTimestamp2 = appMetadata2.GetExecutionInstanceTimestamp();
+
+            // Assert
+            executionInstanceTimestamp1.ShouldBeEquivalentTo(executionInstanceTimestamp2);
+        }
+
+        [Fact]
+        public void MachineLocalTimeZone_should_stay_constant_over_instances()
+        {
+            // Arrange
+            AppMetadata<AppMetadataTests> appMetadata1 =
+                new AppMetadata<AppMetadataTests>("DoesNotMatter1");
+
+            AppMetadata<AppMetadataTests> appMetadata2 =
+                new AppMetadata<AppMetadataTests>("DoesNotMatter2");
+
+            // Act
+            var timeZone1 = appMetadata1.GetMachineLocalTimeZone();
+            var timeZone2 = appMetadata2.GetMachineLocalTimeZone();
+
+            // Assert
+            timeZone1.ShouldBeEquivalentTo(timeZone2);
+        }
+    }
+}
--- src/NetChris.Core/NetChris.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NetChris.Core
-{
-    public class Class1
-    {
-    }
-}
