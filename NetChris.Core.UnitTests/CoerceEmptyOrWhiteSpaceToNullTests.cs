83f79e2f3a423a85a1308f9e3d6df0397c51d85c
Chris Simmons <gitlab.com@netchris.com>
2024-05-18 13:42:39 -0700

    StringExtensions.CoerceEmptyOrWhiteSpaceToNull (#9)
    - Removed ncrunchproject

---
+++ NetChris.Core.UnitTests/CoerceEmptyOrWhiteSpaceToNullTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+
+namespace NetChris.Core.UnitTests;
+
+public class CoerceEmptyOrWhiteSpaceToNullTests
+{
+    [Fact]
+    public void EmptyStringResultsInNull()
+    {
+        string.Empty.CoerceEmptyOrWhiteSpaceToNull().Should().BeNull();
+    }
+    
+    [Fact]
+    public void OnlySpacesResultInNull()
+    {
+        "  ".CoerceEmptyOrWhiteSpaceToNull().Should().BeNull();
+    }
+    
+    [Fact]
+    public void OnlyTabsResultInNull()
+    {
+        "\t\t".CoerceEmptyOrWhiteSpaceToNull().Should().BeNull();
+    }
+    
+    [Fact]
+    public void OnlyNewlinesResultInNull()
+    {
+        "\n\n".CoerceEmptyOrWhiteSpaceToNull().Should().BeNull();
+    }
+    
+    [Fact]
+    public void NonWhitespaceValueComesThroughUnmolested()
+    {
+        "Some non-whitespace value".CoerceEmptyOrWhiteSpaceToNull().Should().Be("Some non-whitespace value");
+    }
+    
+    [Fact]
+    public void UntrimmedNonWhitespaceValueComesThroughUnmolested()
+    {
+        " Untrimmed non-whitespace value   ".CoerceEmptyOrWhiteSpaceToNull(false)
+            .Should().Be(" Untrimmed non-whitespace value   ");
+    }
+    
+    [Fact]
+    public void TrimmedNonWhitespaceValueComesThroughTrimmed()
+    {
+        " Untrimmed non-whitespace value   ".CoerceEmptyOrWhiteSpaceToNull(true)
+            .Should().Be("Untrimmed non-whitespace value");
+    }
+}
---
+++ NetChris.Core/StringExtensions.cs
@@ -0,0 +1,29 @@
+namespace NetChris.Core;
+
+/// <summary>
+/// Extension methods for <see cref="string"/>.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Given a string, returns <c>null</c> if <paramref name="value"/> is null, empty, or whitespace.  Otherwise, the string is returned as-is.
+    /// </summary>
+    /// <param name="value">The value</param>
+    /// <param name="trimValue">Whether to trim the result if non-null, non-whitespace.  Defaults to <c>false</c></param>
+    /// <returns><c>null</c> if <paramref name="value"/> is null, empty, or whitespace.  Otherwise, the string is returned as-is (with optional trimming)</returns>
+    public static string? CoerceEmptyOrWhiteSpaceToNull(this string? value, bool trimValue = false)
+    {
+        var result = value;
+
+        if (string.IsNullOrWhiteSpace(result))
+        {
+            result = null;
+        }
+        else if (trimValue)
+        {
+            result = result!.Trim();
+        }
+
+        return result;
+    }
+}
