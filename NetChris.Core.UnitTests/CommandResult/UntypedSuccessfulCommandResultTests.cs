679a2ced0d899c68cc7387786c7a55557e2358bc
Chris Simmons <gitlab.com@netchris.com>
2025-05-06 21:49:45 -0700

    Obsolete tests

--- NetChris.Core.UnitTests/CommandResult/UntypedSuccessfulCommandResultTests.cs
+++ NetChris.Core.UnitTests/CommandResult/UntypedSuccessfulCommandResultTests.cs
@@ -2,6 +2,7 @@ using NetChris.Core.CommandResult;
 
 namespace NetChris.Core.UnitTests.CommandResult;
 
+[System.Obsolete("ICommandResult is obsolete")]
 public class UntypedSuccessfulCommandResultTests : SuccessfulCommandResultTests
 {
     protected override ICommandResult CommandResult => new SuccessfulCommandResult();
