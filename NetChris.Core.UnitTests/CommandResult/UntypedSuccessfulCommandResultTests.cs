cbda75785c57b4df9b8cc9e1fb070ecc1e29d5fc
Chris Simmons <gitlab.com@netchris.com>
2025-05-10 18:30:43 -0700

    Remove all CommandResult stuff

--- NetChris.Core.UnitTests/CommandResult/UntypedSuccessfulCommandResultTests.cs
+++
@@ -1,9 +0,0 @@
-using NetChris.Core.CommandResult;
-
-namespace NetChris.Core.UnitTests.CommandResult;
-
-[System.Obsolete("ICommandResult is obsolete")]
-public class UntypedSuccessfulCommandResultTests : SuccessfulCommandResultTests
-{
-    protected override ICommandResult CommandResult => new SuccessfulCommandResult();
-}
